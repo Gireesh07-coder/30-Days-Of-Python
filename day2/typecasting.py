df4feb120a09423c0c4d348b60303c47ca678af5
Gireesh <your@email.com>
2026-06-15 11:46:13 +0530

    Completed Day -02

---
+++ day2/typecasting.py
@@ -0,0 +1,68 @@
+# # Exercises: Level 2
+# # Check the data type of all your variables using type() built-in function
+# # print(type(5))
+# # print(type(5.45))
+# # print(type("Gireesh"))
+# # print(type([12,23,4,5]))
+# # print(type({34,45,6}))
+# # print(type((34,23,21,2)))
+# # print(type({"name":"Gireesh","age":22,"city":"Hirekerur"}))
+# # # Using the len() built-in function, find the length of your first name
+# # print(len('Gireesh'))
+# # Compare the length of your first name and your last name
+# # first_name='Gireesh'
+# # last_name='Bhajantri'
+# # print(len(first_name))
+# # print(len(last_name))
+
+# # if len(first_name)>len(last_name):
+# #     print("First name is longer than last name")
+# # elif len(first_name)<len(last_name):
+# #     print("last name is longer than the first name")
+# # else:
+# #     print("Both have same length!")
+# # Declare 5 as num_one and 4 as num_two
+# num_one=5
+# num_two=4
+# # Add num_one and num_two and assign the value to a variable total
+# total=num_one+num_two
+# print(total)
+# # Subtract num_two from num_one and assign the value to a variable diff
+# diff=num_two-num_one
+# print(diff)
+# # Multiply num_two and num_one and assign the value to a variable product
+# product=num_one*num_two
+# print(product)
+# # Divide num_one by num_two and assign the value to a variable division
+# division=num_one/num_two
+# print(division)
+# # Use modulus division to find num_two divided by num_one and assign the value to a variable remainder
+# remainder=num_two%num_one
+# print(remainder)
+# # Calculate num_one to the power of num_two and assign the value to a variable exp
+# exp=num_one**num_two
+# # Find floor division of num_one by num_two and assign the value to a variable floor_division
+# floor_division=num_two//num_one
+# # The radius of a circle is 30 meters.
+# radius=30
+# # Calculate the area of a circle and assign the value to a variable name of area_of_circle
+# area_of_circle=float(3.14*(radius*radius))
+# print("area of a circle is:",area_of_circle)
+# # Calculate the circumference of a circle and assign the value to a variable name of circum_of_circle
+# circumference=float(2*3.14*radius)
+# print(circumference)
+# # Take radius as user input and calculate the area.
+# radius=int(input("Enter the radius:"))
+# print(float(3.14*radius*radius))
+# # Use the built-in input function to get first name, last name, country and age from a user and store the value to their corresponding variable names
+# first_name=input("Enter your firstname:")
+# print(first_name)
+# last_name=input("Enter your lastname:")
+# print(last_name)
+# age=int(input("Enter your age:"))
+# print(age)
+# city=input("Enter you cityname:")
+# print(city)
+# # Run help('keywords') in Python shell or in your file to check for the Python reserved words or keywords
+
+help('keywords')
