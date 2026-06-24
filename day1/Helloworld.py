da367ba3db0a39bfea211650dc164468e8cb0e48
Gireesh <your@email.com>
2026-06-24 23:38:56 +0530

    Complete Day 3 Python exercises

--- day1/Helloworld.py
+++ day1/Helloworld.py
@@ -1,25 +1,28 @@
-import math
-print("Hello")
+# import math
+# print("Hello")
 
-# 30 days of python challenge -Day1
-print(2+5)  #addition(+)
-print(5-3)  #subtraction(-)
-print(5*3)  #multiplication(*)
-print(10/2) #division(/)
-print(2**5) #exponentiation(**)
-print(10%2) #modulus(%)
-print(10//2) #floor division(//)
-print(5==5.0) #True
+# # 30 days of python challenge -Day1
+# print(2+5)  #addition(+)
+# print(5-3)  #subtraction(-)
+# print(5*3)  #multiplication(*)
+# print(10/2) #division(/)
+# print(2**5) #exponentiation(**)
+# print(10%2) #modulus(%)
+# print(10//2) #floor division(//)
+# print(5==5.0) #True
 
-#checking data types
-print(type(2))   #Int
-print(type(3.45)) #float
-print(type("Gireesh")) #string
-print(type(5-5j))     #Complex
-print(type([1,2,3]))  #List
-print(type({'name':"Gireesh"}))  #Dictionary
-print(type({2,3,4,5}))    #set
-print(type((3,45,5.4,4)))  #Tuple
-print(type(5==5.0))        #boolean
+# #checking data types
+# print(type(2))   #Int
+# print(type(3.45)) #float
+# print(type("Gireesh")) #string
+# print(type(5-5j))     #Complex
+# print(type([1,2,3]))  #List
+# print(type({'name':"Gireesh"}))  #Dictionary
+# print(type({2,3,4,5}))    #set
+# print(type((3,45,5.4,4)))  #Tuple
+# print(type(5==5.0))        #boolean
 
-print(math.sqrt((10-2)**2-(8-3)**2))
+# print(math.sqrt((10-2)**2-(8-3)**2))
+
+special_characters="!@#$%^&*()-_=+[]{};:'\",.<>?/\\|`~"
+print(len(special_characters))
